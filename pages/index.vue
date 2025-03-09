e3c3127b8101bfcc2354a4e64b1532fd458b6f85
Jerry Lott <lottjerry@gmail.com>
2025-03-08 21:42:44 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-blue-600">Hello</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,vue}",
+    "./components/**/*.{js,vue}",
+    "./layouts/**/*.vue",
+    "./plugins/**/*.{js,vue}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
