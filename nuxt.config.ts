4b807fc9610bda26a357242b938b6e314b10d553
Jerry Lott <lottjerry@gmail.com>
2025-03-11 15:50:50 -0500

    Added gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@hypernym/nuxt-gsap'],
+
+  gsap: {
+    composables: true,
+    provide: false,
+    extraPlugins: {
+      scrollTrigger: true
+    }
+  }
 })
