8648243cae289f04ed946367084777ecce87986f
Jerry Lott <lottjerry@gmail.com>
2025-03-18 18:30:48 -0500

    Fixed layout for about and index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <slot />
+  <div class="flex flex-col justify-between items-center bg-gray-500 h-dvh">
+    <header class="w-full bg-black text-white flex justify-center">
+      <Logo />
+      <nav class="flex w-1/6 justify-around items-center">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+      </nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer class="bg-black text-white w-full flex justify-center">Footer</footer>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,5 @@
 <template>
-	<div class="flex justify-center items-center inset-0 fixed bg-gray-900">
-		<div class="border p-5 flex-col justify-center items-center">
-			<h1 class="text-3xl text-purple-600">About</h1>
-			<NuxtLink class="text-white" to="/">Home Page</NuxtLink>
-		</div>
-	</div>
+  <main>About Page</main>
 </template>
 
 <script setup></script>
