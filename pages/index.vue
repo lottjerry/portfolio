95eadd74276af0e95c0ecc995ff9d0dcf3dd6728
Jerry Lott <lottjerry@gmail.com>
2025-03-19 03:39:59 -0500

    Deleted unwanted files and a few lines of code

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <main ref="main" class="text-5xl text-white">Home Page</main>
+  <main  class="text-5xl text-white">Home Page</main>
 </template>
 
-<script setup></script>
+<script setup>
+
+</script>
 
 <style lang="scss" scoped></style>
