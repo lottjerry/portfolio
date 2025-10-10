cdeaede801222925443d69c58816330981559aa5
Jerry Lott <lottjerry@gmail.com>
2025-10-10 11:36:46 -0500

    Added swiperjs with custom image slides.

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,31 +1,85 @@
 <template>
-  <div class="h-dvh w-dvw overflow-hidden font-ppneue">
-    <nav class="fixed left-0 top-0 z-10 flex w-dvw justify-between p-11">
-      <p class="relative -top-3 text-5xl font-medium uppercase text-black font-timmons">
+  <div class="font-ppneue relative flex h-dvh w-dvw flex-col overflow-hidden">
+    <!-- Top Navigation -->
+    <nav class="fixed left-0 top-0 z-20 flex w-full justify-between p-11">
+      <p
+        class="font-timmons relative -top-3 text-5xl font-medium uppercase text-black"
+      >
         Jerry Lott
       </p>
       <p class="font-medium uppercase text-black">Resume</p>
     </nav>
-    <footer class="fixed bottom-0 left-0 z-10 flex w-dvw justify-between p-11">
+
+    <!-- Background Overlay (behind Swiper) -->
+    <div
+      ref="bgOverlay"
+      class="fixed left-0 top-0 z-0 flex h-dvh w-dvw items-center justify-center bg-[rgb(213,183,71)] opacity-50 brightness-75"
+    >
+      <div>
+        <button
+          @click="swapBackgroundColor()"
+          class="rounded bg-white px-4 py-2 shadow"
+        >
+          Swap Background Color
+        </button>
+      </div>
+    </div>
+
+    <!-- Main Content -->
+    <div
+      class="z-10 flex h-dvh w-dvw flex-1 flex-col items-center justify-center space-y-6"
+    >
+      <!-- Custom Pagination Bullets ABOVE the slider -->
+      <div class="custom-swiper-pagination"></div>
+
+      <!-- Swiper Slider -->
+      <Swiper :modules="modules" :pagination="pagination" class="h-1/2 w-full">
+        <SwiperSlide class="swiper-slide">
+          <img src="/assets/images/img1.jpg" alt="" class="w-1/2" />
+        </SwiperSlide>
+        <SwiperSlide
+          class="swiper-slide"
+        >
+          <img src="/assets/images/img2.jpg" alt="" class="w-1/2" />
+        </SwiperSlide>
+        <SwiperSlide
+          class="swiper-slide"
+        >
+          <img src="/assets/images/img3.jpg" alt="" class="w-1/2" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+
+    <!-- Footer -->
+    <footer class="fixed bottom-0 left-0 z-20 flex w-full justify-between p-11">
       <p class="font-medium uppercase text-black">
         © 2025 Jerry Lott Portfolio
       </p>
       <p class="font-medium uppercase text-black">Social Media Links</p>
     </footer>
-    <div
-      ref="bgOverlay"
-      class="fixed left-0 top-0 flex h-dvh w-dvw items-center justify-center bg-[rgb(213,183,71)] opacity-50 brightness-75"
-    >
-      <div>
-        <button @click="swapBackgroundColor()">Swap Background Color</button>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
   import gsap from 'gsap';
   import CustomEase from 'gsap/CustomEase';
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import { Pagination } from 'swiper/modules';
+
+  // Import Swiper styles
+  import 'swiper/css';
+  import 'swiper/css/pagination';
+
+  // Swiper config
+  const modules = [Pagination];
+
+  const pagination = {
+    el: '.custom-swiper-pagination', // Connect to our manual container
+    clickable: true,
+    renderBullet(index, className) {
+      return `<span class="${className} w-4 h-4 bg-gray-300 rounded-full inline-block"></span>`;
+    },
+  };
 
   const bgOverlay = ref(null);
 
@@ -58,4 +112,20 @@
   });
 </script>
 
-<style></style>
+<style>
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  /* Add this to your <style> section */
+  .swiper-pagination-bullet {
+    @apply mx-1;
+    transition: background-color 0.3s;
+  }
+
+  .swiper-pagination-bullet-active {
+    @apply bg-black;
+  }
+</style>
