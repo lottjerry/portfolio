6686ae9184cdd01c45944f743fbbb6cf48388944
Jerry Lott <lottjerry@gmail.com>
2025-10-15 01:55:18 -0500

    Added logo and about to nav with animations finished.

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,12 +1,57 @@
 <template>
   <nav class="fixed left-0 top-0 flex w-full justify-between p-8">
-    <h1 class="font-timmons text-5xl uppercase">Jerry</h1>
-    <div>Carousel nav goes here.</div>
-    <h2 class="text-lg font-medium">About</h2>
+    <!-- LOGO -->
+    <h1 class="font-timmons text-5xl uppercase">
+      <span class="letter-wrapper"><span>J</span></span>
+      <span class="letter-wrapper"><span>e</span></span>
+      <span class="letter-wrapper"><span>r</span></span>
+      <span class="letter-wrapper"><span>r</span></span>
+      <span class="letter-wrapper"><span>y</span></span>
+    </h1>
+    <!-- PAGINATION -->
+    <div>Pagination goes here.</div>
+    <!-- ABOUT -->
+    <h2 class="text-lg font-medium uppercase">
+      <span class="letter-wrapper"><span>J</span></span>
+      <span class="letter-wrapper"><span>e</span></span>
+      <span class="letter-wrapper"><span>r</span></span>
+      <span class="letter-wrapper"><span>r</span></span>
+      <span class="letter-wrapper"><span>y</span></span>
+    </h2>
   </nav>
 </template>
 
 <script setup>
+  import gsap from 'gsap';
+  import { useAppStore } from '@/stores/appStore';
+
+  const appStore = useAppStore();
+  const { pageLoaded } = storeToRefs(appStore);
+
+
+  // Watch pageLoaded for changes. If change run the gsap animation. 
+  watch(pageLoaded, (val) => {
+    if (val) {
+      gsap.to(['.letter-wrapper span'], {
+        x: '0%',
+        opacity: 1,
+        duration: 1.2,
+        ease: 'power4.out',
+      });
+    }
+  });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  .letter-wrapper {
+    display: inline-block;
+    height: 2em;
+    overflow: hidden;
+  }
+
+  .letter-wrapper span {
+    display: inline-block;
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+</style>
